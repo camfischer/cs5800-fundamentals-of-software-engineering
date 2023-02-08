3b5fb9012eb9d3d623f7721b7365931923a88e2f
camfischer <cam.fisch@outlook.com>
2023-02-08 00:26:15 -0600

    Preparing iter0

---
+++ Iter0.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class Iter0 {
+
+    public static void main(String[] args) {
+        String url
+            = "jdbc:mysql://localhost:3306/exam"; // table details
+        String username = "root"; // MySQL credentials
+        String password = "*";
+        String query
+            = "select *from branch"; // query to be run // Driver name
+        try {
+            Connection con = DriverManager.getConnection(
+                url, username, password);
+                System.out.println(
+                    "Connection Established successfully");
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(query); // Execute query
+                rs.next();
+                String name
+                    = rs.getString("City"); // Retrieve name from db
+         
+                System.out.println(name); // Print result on console
+                st.close(); // close statement
+                con.close(); // close connection
+                System.out.println("Connection Closed....");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       
+
+        System.out.println(sum(1,2));
+    }
+
+    public static int sum(int x, int y){
+        return x + y;
+    }
+}
---
+++ Iter0Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Iter0Test{
+
+    @Test
+    public void test(){
+        assertEquals(3, Iter0.sum(2, 1));
+    }
+}
